4f308fa38993afefdaefb70f1aa3be45c63ae03b
Naimad94 <55095623+Naimad94@users.noreply.github.com>
2019-09-16 12:52:19 -0400

    Update Book.java
    I did everything up to 88.

--- Book.java
+++ Book.java
@@ -6,21 +6,62 @@
  * @author (Insert your name here.)
  * @version (Insert today's date here.)
  */
-class Book
+public class Book
 {
      // The fields.
     private String author;
     private String title;
+    private int pages;
+    private int ref;
+    
+    
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookpages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookpages;
     }
 
-    // Add the methods here ...
+    //** accessor method for author field.*/
+    public String getAuthor()
+    {
+        return author;
+    }
+    //** accessor method for author field.*/
+    public String getTitle()
+    {
+        return title;
+    }
+    //** accessor method for pages field.*/
+    public int getPages()
+    {
+        return pages;
+    }
+    //** displays the authors name.*/
+    public void printAuthor()
+    {
+        System.out.println("Author: " + author);
+    }
+    //** displays the books title.*/
+    public void printTitle()
+    {
+        System.out.println("Title: " + title);
+    }
+    //**displays the books details.*/
+    public void printDetails()
+    {
+        System.out.println("Tittle: " + title + ", " + "Author: "
+        + author + ", " + "Pages: " + pages);
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        return ref;
+    }
+    
 }
